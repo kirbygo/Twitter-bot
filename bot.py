87d6418abbbba3ca7f55771ceba254fa6d63a473
Daniel Ruiz <kirbygo@hotmail.com>
2020-02-05 17:01:17 -0600

    Update bot.py
    yalacagué, eso me pasa por no salvar

--- bot.py
+++ bot.py
@@ -1,4 +1,18 @@
 import tweepy
+import json
+
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
+
+    def on_status(self, status):
+        if status.retweeted_status:
+            return
+        print(f"{tweet.user.name}:{tweet.text}")
+
+    def on_error(self, status):
+        print("Error, putito...")
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler("rCrxISeNNSzeAkZiTA4nrk1fX", "sppCdj4Txbwm7VisaFcXcyqADQaywi5X6FTTvvnRIT1W5ZHnzI")
@@ -14,5 +28,15 @@ try:
 except:
     print("Error during authentication")
 
-# Create a tweet
-api.update_status("Ando aprendiendo Tweepy, a ver si sí jala :P")
+tweets_listener = MyStreamListener(api)
+stream = tweepy.Stream(api.auth, tweets_listener)
+stream.filter(track=["en base a"], languages=["es"])
+
+
+
+# Crear un tweet
+# api.update_status("Ando aprendiendo Tweepy, a ver si sí jala :P")
+
+# Buscar tweets
+# for tweet in api.search(q="Python", lang="en", rpp=10):
+#    print(f"{tweet.user.name}:{tweet.text}")
