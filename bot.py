cf1d20c0f15f914c19797ecfaa9cb3c6bccff22a
Daniel Ruiz <kirbygo@hotmail.com>
2020-02-05 15:08:15 -0600

    Update bot.py
    j

--- bot.py
+++ bot.py
@@ -7,5 +7,11 @@ auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
 # Create API object
 api = tweepy.API(auth)
 
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
+
 # Create a tweet
 api.update_status("Hello Tweepy")
