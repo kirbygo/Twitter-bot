7357513c553c82d995ccc54acb38b719e8cff4c6
Daniel Ruiz <kirbygo@hotmail.com>
2020-02-05 15:18:01 -0600

    Update bot.py
    a ver si jala

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import tweepy
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+auth = tweepy.OAuthHandler("rCrxISeNNSzeAkZiTA4nrk1fX", "sppCdj4Txbwm7VisaFcXcyqADQaywi5X6FTTvvnRIT1W5ZHnzI")
+auth.set_access_token("82220649-OGt1O8Jv0L6IwZmmUwcjc4Z3br7PqCwU0DtG3gzxO", "3gNQGaxN9QlC7x1poxqb8Uin6lSGcOBfAuIcJZHAHYUS5")
 
 # Create API object
-api = tweepy.API(auth)
+api = tweepy.API(auth,  wait_on_rate_limit=True,
+    wait_on_rate_limit_notify=True)
 
 try:
     api.verify_credentials()
@@ -14,4 +15,4 @@ except:
     print("Error during authentication")
 
 # Create a tweet
-api.update_status("Hello Tweepy")
+api.update_status("Ando aprendiendo Tweepy, a ver si sí jala :P")
